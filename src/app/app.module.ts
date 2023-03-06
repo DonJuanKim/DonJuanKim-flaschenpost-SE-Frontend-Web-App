2521000669553f958d940d199540af568a0a8780
Donghwan Kim <donghwan.kim92@gmail.com>
2023-03-06 20:42:26 +0100

    Added 3 components with routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { BottleViewComponent } from './bottle-view/bottle-view.component';
+import { CartComponent } from './cart/cart.component';
 
 // const routes: Routes = [{ path: 'cart', component: cartComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BottleViewComponent,
+    DetailViewComponent,
+    CartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'detail-view', component: DetailViewComponent },
+      { path: 'bottle-view', component: BottleViewComponent },
+      { path: 'cart', component: CartComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
